b69611a82830b87684fce1c49122e6d3f56238b7
Birendra Bohara <bbirendra693@gmail.com>
2025-11-30 15:34:27 +0545

    Learned type conversion, parseInt/parseFloat, toFixed, isNaN, isInteger

--- number/level2.js
+++ number/level2.js
@@ -19,3 +19,44 @@ console.log("Round Nearest 7.49: " + roundNearest);
 console.log("Largest Number: " + largest);
 console.log("Smallest Number: " + smallest);
 console.log("Random Number between 0 and 1: " + randomNum);
+
+// Q14. Convert the string "150" to a number.
+let str= "150"
+console.log(Number(str));  // 150
+
+// Q15. Convert the string "99.99" to a decimal number.
+let str1="99.99"
+console.log(parseFloat(str1));  // 99.99
+
+// Q16. Convert the string "200px" to an integer.
+let str2="200px"
+console.log(parseInt(str2));  // 200
+
+// Q17. Convert the boolean true to a number.
+let boolValue1= true;
+let boolValue2= false;
+console.log(Number(boolValue1));   // 1
+console.log(Number(boolValue2));  // 2
+
+// Q18. Format the number 45.4567 to 2 decimal places.
+let num = 45.4567;
+console.log(num.toFixed(3));  // 45.46
+
+// Q19. Convert the number 500 to a string.
+let num1= 500;
+console.log(typeof(num1));   // number
+let output=num1.toString();
+console.log(output);         // 500
+console.log(typeof(output));  // string
+
+// Q20. Check if "abc" is Not a Number.
+let value= "abc";
+console.log(isNaN(value));   // true
+
+// Q21. Check if NaN is really NaN. 
+let value1= NaN;
+console.log(Number.isNaN(value1));   // true
+
+// Q23. Check if 25.5 is an integer. 
+let value3= 25.5;
+console.log(Number.isInteger(value3));  // false
