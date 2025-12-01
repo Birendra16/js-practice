f6cf676abbfdff65e295b1a72cc2f4e9d05e16ab
Birendra Bohara <bbirendra693@gmail.com>
2025-12-01 21:02:09 +0545

    boolean practice

---
+++ boolean/level1.js
@@ -0,0 +1,10 @@
+const num1=5
+const num2=10
+
+// Q1 Check if num1 is greater than num2 output should be false
+console.log(num1>num2);
+
+const lang1='javascript'
+const lang2='typescript'
+//Q2 check if both lang1 and lang2 have length, output should be true
+console.log(lang1.length===lang2.length);
