723a51990d119b2b75fbfbc8238652cf2746a693
Birendra Bohara <bbirendra693@gmail.com>
2025-12-21 05:59:20 +0545

    remaining boolean level 1 qns solved

--- boolean/level1.js
+++ boolean/level1.js
@@ -1,10 +1,36 @@
 const num1=5
 const num2=10
 
-// Q1 Check if num1 is greater than num2 output should be false
-console.log(num1>num2);
+// Q1. Check if num1 is greater than num2 output should be false
+console.log(num1>num2); // false
 
 const lang1='javascript'
 const lang2='typescript'
-//Q2 check if both lang1 and lang2 have length, output should be true
-console.log(lang1.length===lang2.length);
+//Q2. check if both lang1 and lang2 have length, output should be true
+console.log(lang1.length===lang2.length); // true
+
+// Q3.	Check if 100 == "100".
+        console.log(100=="100"); // true
+
+// Q4.	Check if 100 === "100".
+        console.log(100==="100"); // false
+
+// Q5.	Check if true is equal to 1.
+        console.log(true==1); //  true
+        console.log(true===1); // false
+
+// Q6.	Check if false is equal to 0.
+        console.log(false==0); // true
+        console.log(false===0); //false
+
+// Q7.	Determine the Boolean value of "" (empty string).
+        console.log(Boolean("")); // false
+
+// Q8.	Determine the Boolean value of "Hello".
+        console.log(Boolean("Hello")); // true
+
+// Q9.	Determine the Boolean value of 0.
+        console.log(Boolean(0)); // false
+
+// Q10.	Determine the Boolean value of 1.
+        console.log(Boolean(1)); // true
